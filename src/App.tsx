41b87eaf825ff741009f0d0e4bbaa536b478305b
monic moseka <sarahmosky@gmail.com>
2024-09-16 09:20:34 +0100

    nouveau commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import NavMenu from './components/nav/navMenu';
 import SectionInput from './components/sectionInput/sectionInput'
-import TrendingLists from './components/trends/trendingLists';
 
                                                     
 function App() {
@@ -9,11 +8,10 @@ function App() {
   return (
    <div className='flex'>
 
-      
       <NavMenu />
       <SectionInput/>
-      
-      < TrendingLists/>
+    
+     
    </div>
     
     
